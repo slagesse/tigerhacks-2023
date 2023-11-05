cf91bd566b9f8b7c904f007fdee798a884de2860
slagesse <shanelagesse@gmail.com>
2023-11-05 09:50:29 -0600

    final

--- server.js
+++ server.js
@@ -29,6 +29,10 @@ app.get("/index.html", function(req, res) {
     res.sendFile(__dirname + "/index.html")
   });
 
+app.get("/about.html", function(req, res) {
+  res.sendFile(__dirname + "/about.html")
+});
+
 //Login -> login.html
 app.get("/login.html", function(req, res) {
     res.sendFile(__dirname + "/login.html")
